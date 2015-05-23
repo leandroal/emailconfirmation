b78a5a14e51657080123145134ecaf65af465b7f
DurvalPCN <duduwhitney@gmail.com>
2015-05-23 20:20:05 -0300

    Added server-side files.

---
+++ server-scripts/storeuser.php
@@ -0,0 +1,31 @@
+<?php
+/* E-mail confirmation sample for BlackBerry 10 - Server side
+ * Copyright (C) <2015> Durval Pereira <durval@compelab.org>
+ * Copyright (C) <2015> Leandro Melo de Sales <leandro@compelab.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this code; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+$email = (isset($_POST['email'])) ? $_POST['email'] : '';
+$name = (isset($_POST['name'])) ? $_POST['name'] : '';
+
+$to = $email;
+$subject = "Email Confirmation Sign up";
+$message = "Dear " . $name . ", thanks for signing up for our service. To confirm your account, please click on the url: http://www.compelab.org/emailconfirmation/confirm.php";
+$header = "From:emailconfirmation@compelab.org \r\n";
+mail($to, $subject, $message, $header);
+
+?>
