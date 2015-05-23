2b877f5cdf97dfea4df9cbf5f5ae93371f53e762
Leandro Melo de Sales <leandroal@gmail.com>
2015-05-23 20:21:41 -0300

    Change from library to code

--- server-scripts/storeuser.php
+++ server-scripts/storeuser.php
@@ -3,7 +3,7 @@
  * Copyright (C) <2015> Durval Pereira <durval@compelab.org>
  * Copyright (C) <2015> Leandro Melo de Sales <leandro@compelab.org>
  *
- * This library is free software; you can redistribute it and/or
+ * This code is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
