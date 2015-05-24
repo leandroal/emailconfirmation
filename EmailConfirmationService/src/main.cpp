ab2f5bba647d71d2aef67a13669abd19d0de0721
Dielson Sales <dielsonscarvalho@gmail.com>
2015-05-23 21:51:20 -0300

    Adding license to application source files

--- EmailConfirmationService/src/main.cpp
+++ EmailConfirmationService/src/main.cpp
@@ -1,27 +1,31 @@
-/* Copyright (c) 2013 BlackBerry Limited.
+/* E-mail confirmation sample for BlackBerry 10 - Client side
+ * Copyright (C) <2015> Dielson Carvalho <dielson.carvalho@compelab.org>
+ * Copyright (C) <2015> Durval Pereira <durval@compelab.org>
+ * Copyright (C) <2015> Leandro Melo de Sales <leandro@compelab.org>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU. Library General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU Library General Public
+ * License along with this code; if not, write to the
+
+
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
+
 #include <bb/Application>
 
 #include "applicationheadless.hpp"
 
 using namespace bb;
-
-//void myMessageOutput(QtMsgType type, const char* msg) {
-//    fprintf(stdout, "%s\n", msg);
-//    fflush (stdout);
-//}
 
 int main(int argc, char **argv)
 {
@@ -33,5 +37,4 @@ int main(int argc, char **argv)
 
     // Enter the application main event loop.
     return Application::exec();
-//    qInstallMsgHandler(myMessageOutput);
 }
